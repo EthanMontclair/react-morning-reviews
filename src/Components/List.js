22029a54c828397cd7d080376e3e6f5169fe8ea8
Ethan Montclair <montclair.ethan@gmail.com>
2020-07-22 10:33:23 -0600

    commit

---
+++ src/Components/List.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+
+class List extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            movies: []
+        }
+
+    }
+    
+    render() {
+        return(
+            <h1>This is the List Component</h1>
+        )
+    }
+}
+
+
+export default List
